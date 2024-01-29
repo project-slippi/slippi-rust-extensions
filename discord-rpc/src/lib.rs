5b84e802d8870cd709807ee2212d3de14cd56b87
Anders Madsen <madsenanders97@gmail.com>
2024-01-29 22:07:55 +0100

    Updated lib.rs for discord-rpc
    Removing tokio (partly done) (untested)

--- discord-rpc/src/lib.rs
+++ discord-rpc/src/lib.rs
@@ -3,7 +3,6 @@
 // TODO HRC & BTT Records in discord
 // TODO Ranked match score, button "Viw opponent ranked profile", show details in stage striking already (in discord rich presence, signalize that you are in stage striking as well)
 // TODO clean up melee.rs, move structs/enums away in coherent bundles
-
 //#![windows_subsystem = "windows"]
 #![feature(generic_const_exprs)]
 
@@ -12,9 +11,10 @@ extern crate serde_derive;
 
 use discord::{DiscordClientRequest, DiscordClientRequestType};
 use single_instance::SingleInstance;
-use tokio_util::sync::CancellationToken;
-use tokio::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use util::sleep;
+use std::thread;
+use std::sync::mpsc::TryRecvError;
 
 use crate::tray::MeleeTrayEvent;
 
@@ -25,58 +25,57 @@ mod rank;
 mod util;
 mod melee;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let instance = SingleInstance::new("SLIPPI_DISCORD_RICH_PRESENCE_MTX").unwrap();
     assert!(instance.is_single());
-    let (tx, mut rx) = mpsc::channel::<DiscordClientRequest>(32);
-    let (mtx, mrx) = std::sync::mpsc::channel::<MeleeTrayEvent>();
+    let (tx, rx) = mpsc::channel::<DiscordClientRequest>();
+    let (mtx, mrx) = mpsc::channel::<MeleeTrayEvent>();
 
-    let cancel_token = CancellationToken::new();
-    let melee_cancel_token = cancel_token.child_token();
-    tokio::spawn(async move {
-        loop {
-            let discord_tx = tx.clone();
-            let tray_tx = mtx.clone();
-            let c_token = melee_cancel_token.clone();
-            let res = tokio::task::spawn_blocking(move || {
-                let mut client = melee::MeleeClient::new();
-                client.run(c_token, discord_tx, tray_tx);
-            }).await;
-            match res {
-                Ok(_) => { /* handle successfull exit */ },
-                Err(err) if err.is_panic() => {
-                    // panic
-                    let _ = tx.send(DiscordClientRequest::clear()).await;
-                    println!("[ERROR] Melee Client crashed. Restarting...");
-                    sleep(500);
-                },
-                Err(_) => { }
-            }
-        }
-    });
+    let cancel_token = Arc::new(Mutex::new(false));
 
-    let discord_cancel_token = cancel_token.clone();
-    tokio::spawn(async move {
-        let mut discord_client = discord::start_client().unwrap();
-
-        loop {
-            if discord_cancel_token.is_cancelled() {
-                break
-            }
-            let poll_res = rx.try_recv();
-            if poll_res.is_ok() {
-                let msg = poll_res.unwrap();
-                println!("{:?}", msg);
-                match msg.req_type {
-                    DiscordClientRequestType::Queue => discord_client.queue(msg.scene, msg.character).await,
-                    DiscordClientRequestType::Idle => discord_client.idle(msg.scene, msg.character).await,
-                    DiscordClientRequestType::Game => discord_client.game(msg.stage, msg.character, msg.mode, msg.timestamp, msg.opp_name).await,
-                    DiscordClientRequestType::Mainmenu => discord_client.main_menu().await,
-                    DiscordClientRequestType::Clear => discord_client.clear()
+    {
+        let cancel_token = cancel_token.clone();
+        thread::spawn(move || {
+            while !*cancel_token.lock().unwrap() {
+                let discord_tx = tx.clone();
+                let tray_tx = mtx.clone();
+                // The loop is now managed by a simple spawning of a new thread after a crash
+                match thread::spawn(move || {
+                    let mut client = melee::MeleeClient::new();
+                    client.run(discord_tx, tray_tx);
+                }).join() {
+                    Ok(_) => { /* handle successful exit */ },
+                    Err(_) => {
+                        // panic
+                        let _ = tx.send(DiscordClientRequest::clear());
+                        println!("[ERROR] Melee Client crashed. Restarting...");
+                        sleep(500);
+                    }
                 }
             }
-            
+        });
+    }
+
+    let discord_cancel_token = cancel_token.clone();
+    thread::spawn(move || {
+        let mut discord_client = discord::start_client().unwrap();
+
+        while !*discord_cancel_token.lock().unwrap() {
+            let poll_res = rx.try_recv();
+            match poll_res {
+                Ok(msg) => {
+                    println!("{:?}", msg);
+                    match msg.req_type {
+                        DiscordClientRequestType::Queue => discord_client.queue(msg.scene, msg.character),
+                        DiscordClientRequestType::Idle => discord_client.idle(msg.scene, msg.character),
+                        DiscordClientRequestType::Game => discord_client.game(msg.stage, msg.character, msg.mode, msg.timestamp, msg.opp_name),
+                        DiscordClientRequestType::Mainmenu => discord_client.main_menu(),
+                        DiscordClientRequestType::Clear => discord_client.clear()
+                    }
+                },
+                Err(TryRecvError::Disconnected) => break,
+                Err(TryRecvError::Empty) => {}
+            }
         }
         discord_client.close();
     });
@@ -84,5 +83,5 @@ async fn main() {
     tray::run_tray(mrx); // synchronous
 
     // cleanup
-    cancel_token.cancel();
+    *cancel_token.lock().unwrap() = true;
 }
